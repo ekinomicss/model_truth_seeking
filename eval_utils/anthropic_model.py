8f6024c2ac59e3f730da6ab6403de8f844fad8ad
ekinomicss <zorerekin@gmail.com>
2024-11-17 23:39:52 -0500

    update anthro utils

--- eval_utils/anthropic_model.py
+++ eval_utils/anthropic_model.py
@@ -5,19 +5,18 @@ from anthropic import Anthropic, AsyncAnthropic
 from anthropic._types import NOT_GIVEN
 from anthropic.types import Message, MessageParam
 from .env_vars import ENV
-# from types import ChatMessage
 
 def get_anthropic_client_sync() -> Anthropic:
+    """Get a synchronous Anthropic client."""
     if ENV.ANTHROPIC_API_KEY is None:
         raise Exception("Missing Anthropic API key.")
     return Anthropic(api_key=ENV.ANTHROPIC_API_KEY)
 
-
 def get_anthropic_client_async() -> AsyncAnthropic:
+    """Get an asynchronous Anthropic client."""
     if ENV.ANTHROPIC_API_KEY is None:
         raise Exception("Missing Anthropic API key.")
     return AsyncAnthropic(api_key=ENV.ANTHROPIC_API_KEY)
-
 
 def get_anthropic_chat_completion(
     client: Anthropic,
@@ -53,8 +52,10 @@ async def get_anthropic_chat_completion_async(
         temperature=temperature
     )
 
-
 def parse_anthropic_completion(response: Message | None) -> str | None:
+    """
+    Parse the response from Anthropic API to extract the text content.
+    """
     if not response:
         return None
     try:
